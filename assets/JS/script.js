d918626075716a0579b32a5aad4596e090e55102
Noah Blackman <nablackm@usc.edu>
2021-09-29 11:27:01 -0400

    was able to get data by city from API

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,16 +6,6 @@ var lastSearches = [];
 function saveLastSearch() {
 
     localStorage.setItem("recentCitySearches", JSON.stringify(lastSearches));
-
-    // var searchInputVal = document.querySelector("#search-input").value;
-    // console.log(searchInputVal.value);
-
-    // if (!searchInputVal) {
-    //     console.error("You need a search input value!!");
-    //     return;
-    // }
-    // lastSearches.push(searchInputVal);
-    // localStorage.setItem("Recent City Searches", JSON.stringify(lastSearches));
 }
 
 searchButton.on("click",function (event) {
@@ -26,10 +16,11 @@ searchButton.on("click",function (event) {
         console.error("You need a search input value!!");
         return;
     }
-    lastSearches.push(searchInputVal);
+    lastSearches.push(searchInputVal.trim());
     console.log(lastSearches);
     document.querySelector("#search-input").value = "";
     saveLastSearch();
+    getDataByCity(searchInputVal.trim());
 });
 
 function init() {
@@ -38,6 +29,26 @@ function init() {
     if (storedSearches !== null ) {
         lastSearches = storedSearches;
     }
+}
+
+function getDataByCity(city) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d954b63ba94c93f074805e508116108b";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data){
+                getDataByCoord(data);
+            }); 
+        } else {
+            alert("Error: "+ response.statusText);
+        }
+    });
+}
+
+function getDataByCoord(cityData) {
+    var lon = cityData.city.coord.lat;
+    var lat = cityData.city.coord.lon;
+    console.log(lon +' : ' + lat);
 }
 
 init();
